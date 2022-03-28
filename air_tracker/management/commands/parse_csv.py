f1a53a12af117c3adf9088f225854ee135124506
topherlee <topher.lee10@gmail.com>
2022-03-28 20:39:19 +0100

    modified parse_csv and model names

---
+++ air_tracker/management/commands/parse_csv.py
@@ -0,0 +1,45 @@
+import csv
+from pathlib import Path
+from django.core.management.base import BaseCommand, CommandError
+from air_tracker.models import Station_details, Dates, Times, Wind_direction, Wind_speed, Temperature, Station_data
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        #drop all tables to prevent duplicates
+        Station_details.objects.all().delete()
+        Dates.objects.all().delete()
+        Times.objects.all().delete()
+        Wind_direction.objects.all().delete()
+        Wind_speed.objects.all().delete()
+        Temperature.objects.all().delete()
+        Station_data.objects.all().delete()
+        print("Table dropped successfully")
+
+        base_dir = Path(__file__).resolve().parent.parent.parent.parent
+        with open(f'{base_dir}/air_tracker/data/air_pollution_data.csv', "r") as f:
+            reader = csv.reader(f, delimiter=",")
+            next(reader)    #skip header
+            next(reader)
+            for row in reader:
+                """
+                station_details = Station_details.objects.create(
+                    site_name = 
+                )
+                """
+
+                dates = Dates.objects.create(date = row[0])
+                dates.save()
+
+                times = Times.objects.create(time = row[1])
+                times.save()
+
+                wind_direction = Wind_direction.objects.create(direction = int(row[15]))
+                wind_direction.save()
+
+                wind_speed = Wind_speed.objects.create(speed = float(row[17]))
+                wind_speed.save()
+
+                temperature = Temperature.objects.create(temp = float(row[19]))
+                temperature.save()
+
+                
--- air_tracker/models.py
+++ air_tracker/models.py
@@ -2,48 +2,48 @@ from django.db import models
 
 # Create your models here.
 
-class station_details(models.Model):
+class Station_details(models.Model):
     site_name = models.CharField(max_length=200)
     site_type = models.CharField(max_length=200)
-    latitude = models.DecimalField
-    longitue = models.DecimalField
-    site_comments = models.TextField
+    latitude = models.DecimalField()
+    longitude = models.DecimalField()
+    site_comments = models.TextField()
 
     def __str__(self):
-        return f'{self.site_name},{self.site_type},{self.latitude},{self.longitue}'
+        return f'{self.site_name},{self.site_type},{self.latitude},{self.longitude}'
 
 
-class dates(models.Model):
-    date = models.DateField
+class Dates(models.Model):
+    date = models.DateField(input_formats=['%d/%m/%Y'])
 
 
-class times(models.Model):
-    time = models.TimeField
+class Times(models.Model):
+    time = models.TimeField()
 
 
-class wind_direction(models.Model):
-    direction = models.IntegerField
+class Wind_direction(models.Model):
+    direction = models.IntegerField()
 
 
-class wind_speed(models.Model):
-    speed = models.DecimalField
+class Wind_speed(models.Model):
+    speed = models.DecimalField()
 
 
-class temperature(models.Model):
-    temp = models.DecimalField
+class Temperature(models.Model):
+    temp = models.DecimalField()
 
 
-class station_data(models.Model):
-    date = models.ForeignKey(dates)
-    time = models.ForeignKey(times)
-    wind_direction = models.ForeignKey(wind_direction)
-    wind_speed = models.ForeignKey(wind_speed)
-    temperature = models.ForeignKey(temperature)
-    nitric_oxide = models.DecimalField
-    nitrogen_dioxide = models.DecimalField
-    nitrogen_oxides = models.DecimalField
-    pm10 = models.DecimalField
-    pm2point5 = models.DecimalField
-    pm1 = models.DecimalField
-    ozone = models.DecimalField
-    station_details ???
+class Station_data(models.Model):
+    date = models.ForeignKey(Dates)
+    time = models.ForeignKey(Times)
+    wind_direction = models.ForeignKey(Wind_direction)
+    wind_speed = models.ForeignKey(Wind_speed)
+    temperature = models.ForeignKey(Temperature)
+    nitric_oxide = models.DecimalField()
+    nitrogen_dioxide = models.DecimalField()
+    nitrogen_oxides = models.DecimalField()
+    pm10 = models.DecimalField()
+    pm2point5 = models.DecimalField()
+    pm1 = models.DecimalField()
+    ozone = models.DecimalField()
+    Station_details ???
