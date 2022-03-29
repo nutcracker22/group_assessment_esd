69f6a94e9c07d36f0970fb7be59e281d7cecdf74
sammiegee <samuelolutola@gmail.com>
2022-03-29 13:03:29 +0100

    made chnages to the following files: views, templates and urls

--- air_tracker/urls.py
+++ air_tracker/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('/data_page', views.data_page, name='data_page'),
 ]
--- air_tracker/views.py
+++ air_tracker/views.py
@@ -1,7 +1,29 @@
 from django.shortcuts import render
+# from .models import Station_data
+# from django.core.paginator import Paginator
+
 
 def home(request):
     context = {
 
     }
     return render(request, 'air_tracker/home.html', context)
+
+
+def login(request):
+    pass
+
+
+def info(request):
+    return render(request, 'air_tracker/info.html')
+
+
+def data_page(request):
+    # data = Station_data.objects.all()
+    # paginator = Paginator(data, 50)
+    # page_number = request.GET.get('page')
+    # page_obj = paginator.get_page(page_number)
+
+    return render(request, 'air_tracker/data.html')
+
+
