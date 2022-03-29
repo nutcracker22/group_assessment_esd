444bc0df60c181a8bc0a4d17f64e5db1190732b5
nutcracker22 <nutcracker@mailbox.org>
2022-03-29 14:02:56 +0100

    added first tests

--- air_tracker/tests.py
+++ air_tracker/tests.py
@@ -1,3 +1,13 @@
-from django.test import TestCase
+from django.test import Client, TestCase
+from django.urls import reverse
+
 
 # Create your tests here.
+class AirTrackerTests(TestCase):
+
+    def test_index_text(self):
+        client = Client()
+        response = client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Hi mates, nice to work with you")
+
