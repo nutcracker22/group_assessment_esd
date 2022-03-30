57eed789df3aecbbe37c86d8fa6aca550b210a11
nutcracker22 <nutcracker@mailbox.org>
2022-03-30 22:00:06 +0100

    added more tests, and refactored some tests

--- air_tracker/tests.py
+++ air_tracker/tests.py
@@ -9,6 +9,12 @@ class AirTrackerIndexTests(TestCase):
     def test_index_text(self):
         client = Client()
         response = client.get('/')
+        stations = Station_details.objects.all()
+        """
+        for station in stations:
+            print(station.site_name)
+            self.assertContains(response, station.site_name)
+        """
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "Station Details")
         self.assertContains(response, "Anderson Drive")
@@ -17,9 +23,7 @@ class AirTrackerIndexTests(TestCase):
         self.assertContains(response, "Market Street 2")
         self.assertContains(response, "Union Street")
         self.assertContains(response, "Wellington Road")
-        stations = Station_details.objects.all()
-        for station in stations:
-            self.assertContains(response, station.site_name)
+        self.assertTemplateUsed(response, 'air_tracker/home.html')
 
 
 class AirTrackerDataTests(TestCase):
@@ -28,8 +32,15 @@ class AirTrackerDataTests(TestCase):
         client = Client()
         response = client.get('/data')
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "Use the station and date filters here")
+        self.assertContains(response, "Kindly select a station to view data")
+        self.assertTemplateUsed(response, 'air_tracker/data.html')
 
+    def test_data_form(self):
+        client = Client()
+        response = client.post('/data', {'station': 6})
+        print(response)
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Wellington Road")
 
 class AirTrackerAirPollutionTests(TestCase):
 
@@ -39,3 +50,4 @@ class AirTrackerAirPollutionTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertContains(response, "About Air Pollution")
         self.assertContains(response, "Related links")
+        self.assertTemplateUsed(response, 'air_tracker/info.html')
