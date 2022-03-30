6136a2d27f19b3fc6803928bed059ce6a03dbcb3
sammiegee <samuelolutola@gmail.com>
2022-03-30 12:01:47 +0100

    added filter to the data page

--- air_tracker/views.py
+++ air_tracker/views.py
@@ -19,11 +19,14 @@ def info(request):
 
 
 def data_page(request):
+    station = None
     data = Station_data.objects.all().filter(station_details_id=1)
     paginator = Paginator(data, 40)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
+    if request.method == "POST":
+        station = request.POST.get('station_name')
 
-    return render(request, 'air_tracker/data.html', {'page_obj': page_obj})
+    return render(request, 'air_tracker/data.html', {'page_obj': page_obj, 'station': station})
 
 
