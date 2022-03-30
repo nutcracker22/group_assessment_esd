7c1e7fd47a4a5b7668ab8495fc32841c88d2e38a
sammiegee <samuelolutola@gmail.com>
2022-03-30 14:26:58 +0100

    fixed station name when displaying table

--- air_tracker/admin.py
+++ air_tracker/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Station_details, Dates, Times, Wind_direction, Wind_speed, Temperature#, Station_data
-# Register your models here.
+from .models import Station_details, Dates, Times, Wind_direction, Wind_speed, Temperature
 
 admin.site.register(Station_details)
 admin.site.register(Dates)
