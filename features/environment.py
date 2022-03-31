cdb5d8e4248c7b84b8ef5641047f8f12424878a5
nutcracker22 <nutcracker@mailbox.org>
2022-03-31 23:07:31 +0100

    small refactoring

--- features/environment.py
+++ features/environment.py
@@ -9,10 +9,13 @@ from django.test.testcases import LiveServerTestCase
 # from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "mysite.settings"
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "esd.settings"
 django.setup()
 
+"""
 # Use the chrome driver specific to your version of Chrome browser and put it in ./driver directory
 # CHROME_DRIVER = os.path.join(os.path.join(os.path.dirname(__file__), 'driver'), 'chromedriver')
 CHROME_DRIVER = os.path.join('driver/chromedriver')
@@ -23,10 +26,15 @@ chrome_options.add_argument("--headless")
 chrome_options.add_argument('--no-proxy-server')
 chrome_options.add_argument("--proxy-server='direct://'")
 chrome_options.add_argument("--proxy-bypass-list=*")
+"""
+
+DRIVER_PATH = os.path.join(os.path.join(os.path.dirname(__file__), 'driver'), 'geckodriver')
+options = FirefoxOptions()
 
 def before_all(context):
     use_fixture(django_test_runner, context)
-    context.browser = webdriver.Chrome(options=chrome_options, executable_path=CHROME_DRIVER)
+    #context.browser = webdriver.Chrome(options=chrome_options, executable_path=CHROME_DRIVER)
+    context.browser = webdriver.Firefox(options=options, executable_path=DRIVER_PATH)
     context.browser.set_page_load_timeout(time_to_wait=200)
 
 def before_scenario(context, scenario):
